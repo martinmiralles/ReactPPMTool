423b0af580ff067e3d2d9daa01cb2a3d14d7321c
Martin Miralles <martinmiralles66@gmail.com>
2019-12-25 15:45:44 -0500

    First commit - created React app

---
+++ src/components/Dashboard.js
@@ -0,0 +1,10 @@
+import React, { Component } from "react";
+
+//A class-based component, via the shortcut 'rcc'
+class Dashboard extends Component {
+  render() {
+    return <h1>Welcome to the Dashboard</h1>;
+  }
+}
+
+export default Dashboard;
