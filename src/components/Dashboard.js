f6ba07ed5810a3a269d7f0852c3aaa0479b1000b
Martin Miralles <martinmiralles66@gmail.com>
2019-12-25 16:32:03 -0500

    Styled Dashboard, Navbar, and ProjectItem components, and added fontawesome

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -1,19 +1,33 @@
 import React, { Component } from "react";
 import ProjectItem from "./Project/ProjectItem";
 
-//A class-based component, via the shortcut 'rcc'
+//A className-based component, via the shortcut 'rcc'
 //All information here will be send to 'App.js', then send to 'index.js'
 
 class Dashboard extends Component {
   render() {
     return (
       //JSX rule: can only return 1 parent div.  The code below wouldn't work without the div below this line
-      <div>
-        <h1 className='alert alert-warning'>Welcome to the Dashboard</h1>
-        <ProjectItem></ProjectItem>
-        <ProjectItem></ProjectItem>
-        <ProjectItem></ProjectItem>
+
+      //Dashboard Component (Project Item included) -->
+      <div className='projects'>
+        <div className='container'>
+          <div className='row'>
+            <div className='col-md-12'>
+              <h1 className='display-4 text-center'>Projects</h1>
+              <br />
+              <a href='ProjectForm.html' className='btn btn-lg btn-info'>
+                Create a Project
+              </a>
+              <br />
+              <hr />
+              <ProjectItem></ProjectItem>
+              <ProjectItem></ProjectItem>
+            </div>
+          </div>
+        </div>
       </div>
+      //End of Dashboard Component
     );
   }
 }
