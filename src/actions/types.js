fd23db86572d4e3c5d60cde4e1270b819b3f32d1
Martin Miralles <martinmiralles66@gmail.com>
2019-12-26 16:48:15 -0500

    Able to get get project by ID

--- src/actions/types.js
+++ src/actions/types.js
@@ -1,4 +1,5 @@
 export const GET_ERRORS = "GET_ERRORS";
 export const GET_PROJECTS = "GET_PROJECTS";
+export const GET_PROJECT = "GET_PROJECT";
 
 //STEP1: after creating a type above, you create a reducer in the 'reducers' folder
