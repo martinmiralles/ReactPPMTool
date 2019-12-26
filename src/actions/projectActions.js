194db87480b1ed9dd4a2b0ee6d72772914d0e86c
Martin Miralles <martinmiralles66@gmail.com>
2019-12-26 18:41:04 -0500

    Added button to dynamically delete projects

--- src/actions/projectActions.js
+++ src/actions/projectActions.js
@@ -1,5 +1,5 @@
 import axios from "axios";
-import { GET_ERRORS, GET_PROJECTS, GET_PROJECT } from "./types";
+import { GET_ERRORS, GET_PROJECTS, GET_PROJECT, DELETE_PROJECT } from "./types";
 
 //STEP 4: must create actions that contain the API calls
 
@@ -7,6 +7,10 @@ export const createProject = (project, history) => async dispatch => {
   try {
     const res = await axios.post("http://localhost:8080/api/project", project);
     history.push("/dashboard");
+    dispatch({
+      type: GET_ERRORS,
+      payload: {}
+    });
   } catch (err) {
     dispatch({
       type: GET_ERRORS,
@@ -34,4 +38,12 @@ export const getProject = (id, history) => async dispatch => {
     //if an invalid URL is manually typed, send user back to the dashboard
     history.push("/dashboard");
   }
+};
+
+export const deleteProject = id => async dispatch => {
+  await axios.delete(`http://localhost:8080/api/project/${id}`);
+  dispatch({
+    type: DELETE_PROJECT,
+    payload: id
+  });
 };
--- src/actions/types.js
+++ src/actions/types.js
@@ -1,5 +1,6 @@
 export const GET_ERRORS = "GET_ERRORS";
 export const GET_PROJECTS = "GET_PROJECTS";
 export const GET_PROJECT = "GET_PROJECT";
+export const DELETE_PROJECT = "DELETE_PROJECT";
 
 //STEP1: after creating a type above, you create a reducer in the 'reducers' folder
