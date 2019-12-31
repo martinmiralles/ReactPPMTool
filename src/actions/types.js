3bb53172e830429329e36297677a63b398019963
Martin Miralles <martinmiralles66@gmail.com>
2019-12-31 11:56:48 -0500

    Created Backlog action types, and reducer for it

--- src/actions/types.js
+++ src/actions/types.js
@@ -4,3 +4,10 @@ export const GET_PROJECT = "GET_PROJECT";
 export const DELETE_PROJECT = "DELETE_PROJECT";
 
 //STEP1: after creating a type above, you create a reducer in the 'reducers' folder
+
+//For SECTION 5: Adding Project Tasks
+//Types for BACKLOG ACTIONS
+
+export const GET_BACKLOG = "GET_BACKLOG";
+export const GET_PROJECT_TASK = "GET_PROJECT_TASK";
+export const DELETE_PROJECT_TASK = "DELETE_PROJECT_TASK";
